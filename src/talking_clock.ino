67a243e1a7be80edcbd7e3f67e8003b483a56d45
bkueffle <bkueffle@uci.edu>
2018-10-30 17:52:03 -0700

    Added source code folder and blank .ino file

---
+++ src/talking_clock.ino
@@ -0,0 +1,21 @@
+/*
+ * ECE 5250
+ * Final Project
+ * Talking clock
+ * 
+ * Benjamin Kueffler
+ * Raymond Mak
+ * Rafi Meguerdijian
+ * Eugene Chiu
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
